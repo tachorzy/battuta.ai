a097a983f52552810ac5e01b8f77225d722df2cb
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-04-30 18:54:21 -0500

    Added custom colors

--- README.md
+++ README.md
@@ -11,5 +11,3 @@
 ![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white)
 
 ![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white)
-
-![OpenAI](https://a11ybadges.com/badge?logo=openai)
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'zellige-50':'#bcdfd1',
+        'zellige-100':'#b7dccd',
+        'zellige-200':'#93cbb4',
+        'zellige-300':'#81c2a8',
+        'zellige-400':'#6eb99b',
+        'zellige-500':'#6dad93',
+        'zellige-600':'#6ba18a',
+        'zellige-700':'#688f7e',
+        'zellige-800':'#678979',
+        'zellige-900':'#5b786a',
+        'zellige-950':'#34453d',
+
+        'medina-200':'#f6eee0',
+        'medina-300':'#f2e8d5',
+        'medina-400':'#e6ddcb',
+        'medina-500':'#d9d1c0',
+        'medina-600':'#cdc6b6',
+        'medina-700':'#c0baab',
+        'medina-800':'#8d8b80',
+        'medina-900':'#5b5d56',
+        'medina-950':'#424641',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':

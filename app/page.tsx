55b52c4c4a55afaeeeaa2204d6d82ba6f1741fdc
tachorzy <tachorzyad@gmail.com>
2023-07-25 22:34:26 -0500

    Added state to SearchBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import SearchBar from "../components/SearchBar";
-import { satoshi, satoshiMedium, satoshiBold } from '../utilS/localFonts';
+import { satoshi, satoshiMedium, satoshiBold } from '../utils/localFonts';
 
 export default function Home() {
   return (
     <main className="bg-medina-100 h-screen pl-24 py-24">
-      <h1 className={satoshiMedium.className + " text-zellige-800 text-6xl py-12"}>Battuta</h1>
+      <h1 className={satoshiMedium.className + " text-zellige-800 text-6xl mb-12"}>Battuta</h1>
       <SearchBar />
     </main>
   )
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,23 +1,46 @@
 "use client";
 
-// import useAutocomplete from '@mui/base/useAutocomplete';
+import React, { useState } from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+import { satoshiMedium } from '../utils/localFonts';
 
+// will replace later with whatever API we use, and eventually the Google Places API.
 const TEST_CITIES = ['Islamabad', 'Lahore', 'Skardu', 'Karachi', 'Peshawar', 'Swat', 'Hunza Valley']
 
 const SearchBar = () => {
+    // hooks 
+    const [destinations, setDestinations] = useState([''])
+    const [startingCity, setStartingCity] = useState('')
+
     return(
-        <div>
+        <div className="flex flex-col gap-y-8">
             <Autocomplete
                 multiple
                 disablePortal
                 id="travel-combo-box"
                 options={TEST_CITIES}
-                sx={{ width: '33%' }}
-                renderInput={(params) => <TextField {...params} label="Combo box" />}
+                className={satoshiMedium.className + " w-1/3"}
+                onChange={(event, newDestinations) => {
+                    setDestinations(newDestinations)
+                }}
+                onInputChange={(event, newDestination) => {
+                setDestinations([...destinations, newDestination])
+                setStartingCity(destinations[0])
+                }}
+                renderInput={(params) => <TextField {...params} label="Choose your path" />}
             />
+            <div className="flex flex-col gap-y-4 w-1/3 h-1/6">
+                {destinations.map((destination) => {
+                    return(
+                        <h1 className="rounded-md p-4 bg-medina-400 text-lg text-medina-950">
+                            {destination}
+                        </h1>
+                    )
+                })}
+            </div>
         </div>
+
     )
 
 

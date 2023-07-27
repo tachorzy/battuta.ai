116eedc4784b423012735bde1f8bd233723b90a4
tachorzy <tachorzyad@gmail.com>
2023-07-27 15:12:12 -0500

    Added NavLogo

--- components/NavLogo.tsx
+++ components/NavLogo.tsx
@@ -3,9 +3,9 @@ import { satoshi, satoshiMedium, satoshiBold } from '@/utils/localFonts';
 
 const NavLogo = () => {
     return(
-        <div className="flex flex-row">
-            <Image src="/icons/noun-camel-3768875-6DAD93.svg" width={150} height={150} className="object-fit" alt="Battuta Logo" />
-            <h1 className="text-neutral-950 text-6xl pt-36 mb-8">Battuta</h1>
+        <div className="flex flex-row relative align-middle">
+                <Image src="/icons/noun-camel-3768875-6DAD93.svg" width={100} height={100} className="object-scale-down" alt="Battuta Logo" />
+                <h1 className={satoshiMedium.className + " text-neutral-700 text-5xl text-bold py-11 align-text-bottom"}>Battuta</h1>
         </div>
     )
 }

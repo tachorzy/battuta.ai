96ac7c64f2ba539908d8081578126babdd93276f
tachorzy <tachorzyad@gmail.com>
2023-07-27 15:07:32 -0500

    Installed prettier for TailwindCSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import NavLogo from "@/components/NavLogo";
 import SearchBar from "@/components/SearchBar";
 import { satoshi, satoshiMedium, satoshiBold } from '@/utils/localFonts';
 
 export default function Home() {
   return (
     <main className="bg-medina-100 h-screen px-36 items-center justify-center">
+      <NavLogo/>
       <h1 className={satoshiMedium.className + " text-zellige-800 text-6xl pt-36 mb-8"}>Battuta</h1>
       <SearchBar/>
 
--- components/NavLogo.tsx
+++ components/NavLogo.tsx
@@ -4,8 +4,8 @@ import { satoshi, satoshiMedium, satoshiBold } from '@/utils/localFonts';
 const NavLogo = () => {
     return(
         <div className="flex flex-row">
-            <Image src="/icons/noun-camel-3768875-6DAD93.svg" width={50} height={50} alt="Battuta Logo" />
-            <h1 className="text-zellige-800 text-6xl pt-36 mb-8">Battuta</h1>
+            <Image src="/icons/noun-camel-3768875-6DAD93.svg" width={150} height={150} className="object-fit" alt="Battuta Logo" />
+            <h1 className="text-neutral-950 text-6xl pt-36 mb-8">Battuta</h1>
         </div>
     )
 }

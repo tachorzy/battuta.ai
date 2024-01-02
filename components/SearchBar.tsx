5bc67ede9fd2bc84e92d6d8f22c2da578074f82a
tachorzy <tachorzyad@gmail.com>
2024-01-01 22:08:05 -0600

    Added rudimentary google maps api code

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import React, { useState } from 'react';
-import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
-import { satoshiMedium } from '../utils/localFonts';
+import { useLoadScript, Autocomplete } from '@react-google-maps/api';
 
+import { satoshiMedium } from '../utils/localFonts';
 // will replace later with whatever API we use, and eventually the Google Places API.
 const TEST_CITIES = ['Islamabad', 'Lahore', 'Skardu', 'Karachi', 'Peshawar', 'Swat', 'Hunza Valley']
 
@@ -12,32 +11,28 @@ const SearchBar = () => {
     const [destinations, setDestinations] = useState([''])
     const [startingCity, setStartingCity] = useState<string | null>("")
 
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.GOOGLE_MAPS_PLACES_API_KEY || '',
+        libraries: ['places'],
+    });
+
     return(
         <div className="flex flex-row">
-            <Autocomplete
-                disablePortal
-                id="travel-combo-box"
-                options={TEST_CITIES}
-                className={satoshiMedium.className + " lg:w-[20%] peer-open:"}
-                onChange={(event, startCity) => {
-                    setStartingCity(startCity)
-                }}
-                renderInput={(params) => <TextField {...params} label="Select starting point" />}
-            />
-            <Autocomplete
-                multiple
-                disablePortal
-                id="travel-combo-box"
-                options={TEST_CITIES.filter(destinationOption => destinationOption !== startingCity)}
-                className={satoshiMedium.className + " lg:w-1/3"}
-                onChange={(event, newDestinations) => {
-                    setDestinations(newDestinations)
-                }}
-                onInputChange={(event, newDestination) => {
-                    setDestinations([...destinations, newDestination])
-                }}
-                renderInput={(params) => <TextField {...params} label="Add Destinations" />}
-            />
+            {isLoaded ?
+                <Autocomplete
+                    onLoad={(autocomplete) => console.log('autocomplete: ', autocomplete)}
+                    onPlaceChanged={() => console.log('place changed')}
+                >
+                    <input
+                        type="text"
+                        placeholder="Starting City"
+                        className={satoshiMedium.className + " bg-transparent border-b border-gray-300 focus:outline-none focus:border-gray-500"}
+                    /> 
+                </Autocomplete>
+                :
+                <p>Google did not load...</p>
+            }
+
         </div>
     )
 
